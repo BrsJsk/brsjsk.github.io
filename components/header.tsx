b77e33ca8aeb661ac0c47769d42cb996a63c8b9c
brsjsk <joskicboris95@gmail.com>
2023-07-11 16:01:40 +0300

    Header changes

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,47 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
 import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
+const navbarItems = [
+  {
+    title: "Home",
+    url: "/",
+    active: false,
+  },
+  {
+    title: "Posts",
+    url: "/posts",
+    active: false,
+  },
+];
+
 const Header = () => {
+  const { asPath } = useRouter();
+
+  const navbarItemsWithActive = useMemo(() => {
+    return navbarItems.map((item) => ({
+      ...item,
+      active: asPath === item.url,
+    }));
+  }, [asPath]);
+
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-2xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8">
-        Boris Joskic
-      </h1>
+      <div>
+        {navbarItemsWithActive.map((item) => (
+          <Link
+            key={item.url}
+            className={`text-xl bold hover:text-blue-600 mr-4 ${
+              item.active ? "text-blue-600" : ""
+            }`}
+            href={item.url}
+          >
+            {item.title}
+          </Link>
+        ))}
+      </div>
+
       <div className="flex flex-row">
         <a href="https://github.com/BrsJsk/">
           <FaGithubSquare className="w-8 h-8 mr-2" />
